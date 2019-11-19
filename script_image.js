28e36aef45ddc3b887f2309cec484fa66339663c
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2019-11-19 09:14:44 +0100

    Corentin <3 <3 <3 <3

--- script_image.js
+++ script_image.js
@@ -3,10 +3,9 @@
 let img_path;
 
 function backgroundModifier(){
-  var start = document.cookie.indexOf('bckg_path:'); //Get the location of the cookie value
-  var stop = document.cookie.indexOf(';'); //Get the end of the cookie value
-
-  var path = document.cookie.substring(stop,start+10);
-  document.body.style.backgroundImage = "url('Game/Scenes/PorteDepart.png')";
+  console.log("toto");
+  var elem = document.getElementById("style");
+  console.log(elem);
+  elem.innerHTML = "html {margin:0;padding:0;background:url(" +'Game/Scenes/PorteDepart.png' +") no-repeat center fixed;-webkit-background-size: cover;background-size: cover;}"
   console.log(document.body.style.backgroundImage);
 };
