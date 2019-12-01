8eb98f43cbee314d8e89e6e630a38a135fb79266
Corentin Le Métayer <clemetayer@enseirb-matmeca.fr>
2019-12-01 18:57:21 +0100

    got cookies values

--- frontend/script_image.js
+++ frontend/script_image.js
@@ -2,7 +2,12 @@
 
 let img_path;
 
-window.onload = backgroundModifier();
+window.onload = LoadScene();
+
+function LoadScene(){
+  backgroundModifier();
+  clickZones();
+}
 
 function backgroundModifier(){
   //let elem = document.getElementById('backg');
@@ -18,7 +23,30 @@ function backgroundModifier(){
   console.log(document.body.style.backgroundImage);
 };
 
-
+function clickZones(){
+  var parent = document.getElementById('clickZones');
+  // parent.insertAdjacentHTML('afterbegin', '<span>Text</span>');
+  const length = parseInt(getCookieValue("nb_click_zones"));
+  console.log(length);
+  const strx1 = "coor_click_x1_";
+  const stry1 = "coor_click_y1_";
+  const strx2 = "coor_click_x2_";
+  const stry2 = "coor_click_y2_";
+  for(var i = 0; i < length; i++){
+    var sx1 = strx1 + i;
+    var sy1 = stry1 + i;
+    var sx2 = strx2 + i;
+    var sy2 = stry2 + i;
+    var x1 = parseFloat(getCookieValue(sx1));
+    var y1 = parseFloat(getCookieValue(sy1));
+    var x2 = parseFloat(getCookieValue(sx2));
+    var y2 = parseFloat(getCookieValue(sy2));
+    console.log(x1);
+    console.log(x2);
+    console.log(y1);
+    console.log(y2);
+  }
+}
 
 function getIndexName(cname,cook){
   var toSearch =cname +"=";
